3b006c50964a9acaaa7abde5d296b4932b333a81
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-19 17:10:39 +0200

    Fixed bug on path

--- test/test_config.sh
+++ test/test_config.sh
@@ -4,4 +4,4 @@ export RUN_ON_START=1
 
 config=${AEROSTACK2_PATH}/simulation/ignition_assets/test/config.json
 
-${AEROSTACK2_PATh}/simulation/ignition_assets/scripts/run_ign.sh ${config}
+${AEROSTACK2_PATH}/simulation/ignition_assets/scripts/run_ign.sh ${config}
