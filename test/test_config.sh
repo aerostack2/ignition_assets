e60640138bc601013da6baf023ae3bb8af0777ba
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-24 10:59:36 +0200

    Fixed crazyflie

--- test/test_config.sh
+++ test/test_config.sh
@@ -2,6 +2,6 @@
 
 export RUN_ON_START=1
 
-config=${AEROSTACK2_PATH}/simulation/ignition_assets/test/config_cf.json
+config=${AEROSTACK2_PATH}/simulation/ignition_assets/test/config.json
 
 ${AEROSTACK2_PATH}/simulation/ignition_assets/scripts/run_ign.sh ${config}
