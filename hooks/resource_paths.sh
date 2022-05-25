a9d1a7bd1af75fd9bc3714744ee57ec20a24c58c
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-25 16:09:28 +0200

    Added hooks

---
+++ hooks/resource_paths.sh
@@ -0,0 +1,2 @@
+ament_prepend_unique_value IGN_GAZEBO_RESOURCE_PATH "$AMENT_CURRENT_PREFIX/share/ignition_assets/models"
+ament_prepend_unique_value IGN_GAZEBO_RESOURCE_PATH "$AMENT_CURRENT_PREFIX/share/ignition_assets/worlds"
